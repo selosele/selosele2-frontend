6fcfb8c3d80c72430928477066e0473ea398e983
selosele <osh3384@naver.com>
2022-09-17 23:04:46 +0900

    Vuex 적용

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import store from './store';
 
 //import '@/assets/scss/style.scss';
 
 const app = createApp(App);
 
-app.use(router).mount('#app');
+app.use(store).use(router).mount('#app');
