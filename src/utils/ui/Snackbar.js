2b003f1c95f2a9306c90f0cabf74fca18096dd7b
selosele <osh3384@naver.com>
2022-09-24 19:02:31 +0900

    메시지 유틸 추가

---
+++ src/utils/ui/Snackbar.js
@@ -0,0 +1,56 @@
+import Swal from 'sweetalert2';
+
+/**
+ * Snackbar 유틸
+ */
+class Snackbar {
+  snackbar = null;
+
+  constructor() {
+    this.snackbar = Swal.mixin({
+      toast: true,
+      position: 'top-end',
+      showConfirmButton: false,
+      timer: 2000,
+    });
+  }
+
+  success(msg) {
+    this.snackbar.fire({
+      icon: 'success',
+      title: msg
+    });
+  }
+
+  error(msg) {
+    this.snackbar.fire({
+      icon: 'error',
+      title: msg
+    });
+  }
+
+  warning(msg) {
+    this.snackbar.fire({
+      icon: 'warning',
+      title: msg
+    });
+  }
+
+  info(msg) {
+    this.snackbar.fire({
+      icon: 'info',
+      title: msg
+    });
+  }
+
+  question(msg) {
+    this.snackbar.fire({
+      icon: 'question',
+      title: msg
+    });
+  }
+}
+
+const snackbar = new Snackbar();
+
+export default snackbar;
