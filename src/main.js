6add9857b2e4216133816bbedb4aa91e0424aa62
selosele <osh3384@naver.com>
2022-09-24 23:05:05 +0900

    vee-validate 적용 중

--- src/main.js
+++ src/main.js
@@ -2,6 +2,24 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import { defineRule } from 'vee-validate';
+
+defineRule('required', value => {
+  if (!value || !value.length) {
+    return '필수 입력 항목입니다.';
+  }
+  return true;
+});
+defineRule('maxLength', (value, [limit]) => {
+  // The field is empty so it should pass
+  if (!value || !value.length) {
+    return true;
+  }
+  if (value.length > limit) {
+    return `${limit}자 이하로 입력하세요.`;
+  }
+  return true;
+});
 
 //import '@/assets/scss/style.scss';
 
--- src/views/AppLogin.vue
+++ src/views/AppLogin.vue
@@ -1,21 +1,48 @@
 <template>
   <div class="login__wrapper">
     <div class="login__inner">
-      <form method="post" action="/a/goto" autocomplete="off" class="login__frm">
-        <input type="text" name="user_id" id="user_id" title="아이디 입력" placeholder="아이디" maxlength="10" />
+      <Form method="post" action="/a/goto" autocomplete="off" class="login__frm" 
+            @submit.prevent="login"
+            :validation-schema="schema"
+            v-slot="{ errors }">
+        <Field type="text" v-model="userId" name="userId" title="아이디 입력" placeholder="아이디" />
+        <p v-if="errors.userId">{{ errors.userId }}</p>
+        
         <!-- 운영모드일 때 비밀번호 input에 minlength="8" maxlength="15" 속성 필요 -->
-        <input type="password" name="user_pw" id="user_pw" title="비밀번호 입력" placeholder="비밀번호" />
-        <input type="hidden" name="_csrf" value="" />
+        <Field type="password" v-model="userPw" name="userPw" title="비밀번호 입력" placeholder="비밀번호" />
+        <p v-if="errors.userPw">{{ errors.userPw }}</p>
+        
+        <!-- <input type="hidden" name="_csrf" value="" /> -->
 
-        <button type="button" class="login__btn--submit">로그인</button>
-      </form>
+        <button type="submit" class="login__btn--submit">로그인</button>
+      </Form>
     </div>
   </div>
 </template>
 
+<script setup>
+import { Form, Field } from 'vee-validate';
+
+const schema = {
+  userId: 'required|maxLength:10',
+  userPw: 'required',
+};
+</script>
+
 <script>
 export default {
   name: 'AppLogin',
+  data() {
+    return {
+      userId: '',
+      userPw: '',
+    }
+  },
+  methods: {
+    login() {
+      alert(11);
+    },
+  },
 };
 </script>
 
