335ce2bf93510f6849a98e84d47181f382f37d20
selosele <osh3384@naver.com>
2022-10-09 14:54:45 +0900

    store.js 디렉터리 구조 변경 및 파일명 변경

--- src/store/index.js
+++ src/store/index.js
@@ -6,14 +6,24 @@ export default createStore({
   state: {
     // 개발모드/운영모드 체크
     isProduction: false,
+    // Token
+    token: null,
     // 공통코드 - 포스트/콘텐츠 만족도조사
     satisCode: [],
     // 블로그 환경설정
     blogConfig: [],
   },
+  getters: {
+    isLogin(state) {
+      return state.token == null ? false : true;
+    },
+  },
   mutations: {
     SET_NODE_ENV(state, isProduction) {
       state.isProduction = isProduction;
+    },
+    SET_TOKEN(state, _token) {
+      state.token = _token;
     },
     SET_SATIS_CODE(state, satisCode) {
       state.satisCode = satisCode;
@@ -23,10 +33,14 @@ export default createStore({
     },
   },
   actions: {
-    async FETCH_NODE_ENV(ctx) {
+    FETCH_NODE_ENV(ctx) {
       ctx.commit('SET_NODE_ENV', (process.env.NODE_ENV === 'production'));
     },
-    async FETCH_CODE(ctx) {
+    FETCH_TOKEN({ commit }, _token) {
+      commit('SET_TOKEN', _token);
+      localStorage.setItem('token', _token);
+    },
+    FETCH_CODE(ctx) {
       // 포스트/콘텐츠 만족도조사
       axios.get('/api/code/list/B01')
         .then(res => {
@@ -35,7 +49,7 @@ export default createStore({
           snackbar.error('오류가 발생했습니다.');
         });
     },
-    async FETCH_BLOG_CONFIG(ctx) {
+    FETCH_BLOG_CONFIG(ctx) {
       axios.get('/api/blogconfig')
         .then(res => {
           ctx.commit('SET_BLOG_CONFIG', res.data);
