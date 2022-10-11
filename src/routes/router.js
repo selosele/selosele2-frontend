8480a030851e669399bf02a4359e16ce2d1af6df
selosele <osh3384@naver.com>
2022-10-11 14:35:18 +0900

    로그인 중인데 로그인페이지 접근시 리다이렉트 처리

--- src/routes/router.js
+++ src/routes/router.js
@@ -11,7 +11,7 @@ import store from '@/store/store';
 
 const routes = [
   ...index,               // 메인
-  ...error,
+  ...error,               // 에러
   ...auth,                // 권한
   ...search,              // 검색
   ...archive,             // 아카이브
@@ -26,11 +26,16 @@ const router = createRouter({
 
 // 로그인 여부에 따른 페이지 접근 처리
 router.beforeEach((to, from, next) => {
+  // 로그인 중인데 로그인 페이지에 접근 시 리다이렉트
+  if (store.getters.isLogin && to.path === '/a/goto') next('/');
+
   if (to.matched.some(record => record.meta.requiresAuth)) {
     if (store.getters.isLogin) {
+      // 로그인 되어 있으면 가던 길 가고
       next();
       return;
     }
+    // 안되어 있으면 에러 페이지로 리다이렉트
     next('/error');
   } else {
     next();
