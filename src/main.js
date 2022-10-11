fb419cb9cf335b29c5c0e100c74b37705ca5e043
selosele <osh3384@naver.com>
2022-10-11 14:34:58 +0900

    공통 computed 설정 및 CSRF Token 세팅

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './routes/router';
 import store from './store/store';
 import axios from 'axios';
 import { initDefineRule } from './validation';
+import { commonComputed } from '@/store/helper';
 
 //import '@/assets/scss/style.scss';
 
@@ -12,6 +13,7 @@ initDefineRule();
 const app = createApp({
   extends: App,
   created() {
+    // JWT Token 변조 감지 시 강제 로그아웃
     const token = localStorage.getItem('token');
     if (token) {
       this.$store.commit('SET_TOKEN', token);
@@ -28,10 +30,26 @@ const app = createApp({
   }
 });
 
+app.mixin({
+  computed: {
+    ...commonComputed
+  },
+});
+
 app.config.globalProperties.$rootUrl = window.location.origin;
 app.config.globalProperties.$http = axios.create({
   baseURL: process.env.VUE_APP_API_URL,
 });
+
+// CSRF Token 세팅
+(async (httpMethods) => {
+  const res = await axios.get(`${process.env.VUE_APP_API_URL}/auth/csrftoken`);
+  for (const method of httpMethods) {
+    axios.defaults.headers[method]['X-CSRF-Token'] = res.data.CSRFToken;
+  }
+})(
+  ['post', 'put', 'delete']
+);
 
 app
   .use(store)
