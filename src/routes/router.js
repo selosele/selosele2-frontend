02d093c013ca0fc478eacfb898f00ca0df53b655
selosele <osh3384@naver.com>
2022-10-15 23:24:51 +0900

    로그인 검증 로직 개선

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router';
-
 import index from './index/index';
 import error from './error/error';
 import auth from './auth/auth';
@@ -8,6 +7,7 @@ import archive from './archive/archive';
 import guestbook from './guestbook/guestbook';
 import blogConfig from './blog-config/blog-config';
 import store from '@/store/store';
+import snackbar from '@/utils/ui/Snackbar';
 
 const routes = [
   ...index,               // 메인
@@ -27,8 +27,12 @@ const router = createRouter({
 // 로그인 여부에 따른 페이지 접근 처리
 router.beforeEach((to, from, next) => {
   // 로그인 중인데 로그인 페이지에 접근 시 리다이렉트
-  if (store.getters.isLogin && to.path === '/a/goto') next('/');
+  if (store.getters.isLogin && to.path === '/a/goto') {
+    snackbar.info('이미 로그인 중입니다.');
+    next('/');
+  }
 
+  // 로그인이 필요한 페이지
   if (to.matched.some(record => record.meta.requiresAuth)) {
     if (store.getters.isLogin) {
       // 로그인 되어 있으면 가던 길 가고
