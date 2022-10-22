1f00066937a7f8f85a8551b97718810522fec059
selosele <osh3384@naver.com>
2022-10-22 19:08:44 +0900

    사이드바 작업

--- src/components/layout/AppSidebar.vue
+++ src/components/layout/AppSidebar.vue
@@ -1,100 +1,108 @@
 <template>
   <aside class="sidebar">
-    <div class="sidebar__item-list sidebar__item-list--tag">
-      <div class="widget widget__box">
-        <h2 class="sidebar__item-title widget__title">
-          <i class="xi-tags" aria-hidden="true"></i>
-          <em>태그</em>
-        </h2>
+    <template v-for="(widget,i) in widgetList" :key="i">
+      <div :class="[
+        'sidebar__item-list',
+        { 'sidebar__item-list--category': widget.id === 1 },
+        { 'sidebar__item-list--tag': widget.id === 2 },
+        ]">
+        <div class="widget widget__box">
+          <h2 class="sidebar__item-title widget__title">
+            <i :class="widget.icon" aria-hidden="true"></i>
+            <em>{{ widget.title }}</em>
+          </h2>
 
-        <ul>
-          <li>
-            <router-link to="/tags/Algorithm" style="font-size: 90%">
-              Algorithm
-              <span>3</span>
-            </router-link>
-          </li>
-          <li>
-            <router-link to="/tags/C">
-              C
-              <span>1</span>
-            </router-link>
-          </li>
-          <li>
-            <router-link to="/tags/iBATIS">
-              iBATIS
-              <span>1</span>
-            </router-link>
-          </li>
-          <li>
-            <router-link to="/tags/Java" style="font-size: 180%">
-              Java
-              <span>6</span>
-            </router-link>
-          </li>
-          <li>
-            <router-link to="/tags/Node.js">
-              Node.js
-              <span>1</span>
-            </router-link>
-          </li>
-          <li>
-            <router-link to="/tags/programmers" style="font-size: 90%">
-              programmers
-              <span>3</span>
-            </router-link>
-          </li>
-          <li>
-            <router-link to="/tags/Spring">
-              Spring
-              <span>2</span>
-            </router-link>
-          </li>
-          <li>
-            <router-link to="/tags/SQL" style="font-size: 90%">
-              SQL
-              <span>3</span>
-            </router-link>
-          </li>
-          <li>
-            <router-link to="/tags/개인 프로젝트">
-              개인 프로젝트
-              <span>1</span>
-            </router-link>
-          </li>
-        </ul>
+          <ul v-if="widget.id === 1">
+            <li v-for="(category,j) in categoryList" :key="j">
+              <router-link :to="`/categories/${category.id}`">
+                {{ category.nm }} <span>{{ category.count }}</span>
+              </router-link>
+            </li>
+          </ul>
+
+          <ul v-if="widget.id === 2">
+            <li v-for="(tag,j) in tagList" :key="j">
+              <router-link
+                :to="`/tags/${tag.id}`"
+                :style="{ fontSize: `${getFontSize(tag.count)}%` }"
+                >
+                {{ tag.nm }} <span>{{ tag.count }}</span>
+              </router-link>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
-
-    <div class="sidebar__item-list sidebar__item-list--category">
-      <div class="widget widget__box">
-        <h2 class="sidebar__item-title widget__title">
-          <i class="xi-library-books-o" aria-hidden="true"></i>
-          <em>카테고리</em>
-        </h2>
-
-        <ul>
-          <li>
-            <router-link to="/categories/개발노트">
-              개발노트
-              <span>9</span>
-            </router-link>
-          </li>
-          <li>
-            <router-link to="/categories/문제풀이">
-              문제풀이
-              <span>3</span>
-            </router-link>
-          </li>
-        </ul>
-      </div>
-    </div>
+    </template>
   </aside>
 </template>
 
 <script>
+import snackbar from '@/utils/ui/Snackbar';
+
 export default {
   name: 'app-sidebar',
+  data() {
+    return {
+      widgetList: [],
+      categoryList: [],
+      tagList: [],
+    }
+  },
+  async created() {
+    // 위젯 목록을 먼저 조회하고
+    await this.listWidget();
+
+    // 카테고리, 태그 목록 및 개수를 동시에 조회한다.
+    await Promise.all([
+      this.listCategoryAndCount(),
+      this.listTagAndCount(),
+    ]);
+  },
+  methods: {
+    // 위젯 목록 조회
+    listWidget() {
+      let params = {
+        useYn: 'Y',
+      };
+
+      return this.$http.get('/widget/list', { params: params } )
+        .then(res => {
+          res.data.map(d => {
+            this.widgetList.push(d);
+          });
+        }).catch(error => {
+          snackbar.error('오류가 발생했습니다.');
+        });
+    },
+    // 카테고리 목록 및 개수 조회
+    listCategoryAndCount() {
+      return this.$http.get('/category/list')
+        .then(res => {
+          res.data.map(d => {
+            this.categoryList.push(d);
+          });
+        }).catch(error => {
+          snackbar.error('오류가 발생했습니다.');
+        });
+    },
+    // 태그 목록 및 개수 조회
+    listTagAndCount() {
+      return this.$http.get('/tag/list')
+        .then(res => {
+          res.data.map(d => {
+            this.tagList.push(d);
+          });
+        }).catch(error => {
+          snackbar.error('오류가 발생했습니다.');
+        });
+    },
+    // font-size 구하기
+    getFontSize(count) {
+      if (count <= 4) return 80;
+      if (count >= 8) return 170;
+      return 25 * count;
+    },
+  },
 };
 </script>
 
