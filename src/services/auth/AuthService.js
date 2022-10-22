41f46852d6b4c049768def4cb32e30391a54e3ab
selosele <osh3384@naver.com>
2022-10-22 13:50:51 +0900

    권한 검증 서비스 수정

--- src/services/auth/AuthService.js
+++ src/services/auth/AuthService.js
@@ -3,12 +3,12 @@
  */
 class AuthService {
   user = null;
-
+  
   constructor() {
     this.user = this.getUser();
   }
 
-  // JWT 토큰으로부터 사용자 정보 받아오기
+  // JWT로부터 사용자 정보 받아오기
   getUser() {
     const token = localStorage.getItem('token');
     if (!token) {
@@ -19,21 +19,22 @@ class AuthService {
 
   // 1개의 권한 검증
   hasRole(role) {
-    for (const r of this.user.roles) {
-      return (role === r ? true : false);
+    for (const { roleId } of this.user.userRole) {
+      return role === roleId;
     }
     return false;
   }
 
   // 파라미터의 모든 권한이 있는지 검증
   hasRoleAll(...roles) {
-    return roles.filter(d => !this.user.roles.includes(d)).length > 0 ? false : true;
+    return roles.every(d => this.user.userRole.filter(r => r.roleId === d).length > 0);
   }
 
   // 파라미터의 권한 중 1개라도 있는지 검증
   hasRoleOr(...roles) {
-    return roles.filter(d => this.user.roles.includes(d)).length > 0 ? true : false;
+    return roles.some(d => this.user.userRole.filter(r => r.roleId === d).length > 0);
   }
+  
 }
 
 const authService = new AuthService();
