efddab6e7c921903b64727e0b056d2ecf9a54330
selosele <osh3384@naver.com>
2022-11-12 17:16:04 +0900

    프론트 빌드 경로 설정

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 const { defineConfig } = require('@vue/cli-service');
+const path = require('path');
+
 module.exports = defineConfig({
+  outputDir: path.resolve(__dirname, '../../backend/selosele2/client'),
   devServer: {
     proxy: {
       '/api': {
