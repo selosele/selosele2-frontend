51384723bb6bb47188cc471421c6d6714f0f2bb3
selosele <osh3384@naver.com>
2023-09-16 13:06:31 +0900

    numeric rule 추가

--- src/utils/validation/validationUtil.js
+++ src/utils/validation/validationUtil.js
@@ -1,13 +1,15 @@
 import { defineRule, configure } from 'vee-validate';
-import { required, min, max, between } from '@vee-validate/rules';
+import { required, min, max, between, numeric } from '@vee-validate/rules';
 import { localize } from '@vee-validate/i18n';
 
+/** 입력 필드 유효성 검사 세팅 */
 export function initDefineRule() {
   
   defineRule('required', required);
   defineRule('min', min);
   defineRule('max', max);
   defineRule('between', between);
+  defineRule('numeric', numeric);
 
   configure({
     generateMessage: localize('ko', {
@@ -16,6 +18,7 @@ export function initDefineRule() {
         min: '0:{length}자 이상 입력하세요.',
         max: '0:{length}자 이하로 입력하세요.',
         between: '0:{min} ~ 1:{max} 사이로 입력하세요.',
+        numeric: '정수만 입력 가능합니다.',
       },
     }),
   });
