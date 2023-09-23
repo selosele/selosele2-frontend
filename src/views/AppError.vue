8dcffa80ece6be714da7a83902f1716226bf61b8
selosele <osh3384@naver.com>
2023-09-23 13:30:06 +0900

    개수별 포스트 조회 API 삭제 및 오류 페이지 UI 개선

--- src/views/AppError.vue
+++ src/views/AppError.vue
@@ -1,28 +1,15 @@
 <template>
   <app-content-wrapper>
     <div class="error__wrapper">
-      <p class="error__message">방문을 원하시는 페이지의 주소가 잘못 입력되었거나, 변경 혹은 삭제되어 페이지를 찾을 수 없습니다.</p>
-
-      <div class="popular-post__list">
-        <h2 class="popular-post__title">이런 글은 어떠신가요?</h2>
-
-        <ul>
-          <template v-if="0 < postList.length">
-            <li class="popular-post__list__item" v-for="(post,i) in postList" :key="i">
-              <router-link :to="`/post/${post.id}`">
-                <p class="popular-post__list__image">
-                  <img :src="post.ogImgUrl" alt="" v-if="post.ogImgUrl">
-                  <span class="popular-post__list__no-image" v-else>{{ post.title.substring(0,1) }}</span>
-                </p>
-                
-                <p class="popular-post__list__title">
-                  <span>{{ post.title }}</span>
-                </p>
-              </router-link>
-            </li>
-          </template>
-        </ul>
+      <div class="error__message">
+        <h2 class="error__title">죄송합니다. 현재 찾을 수 없는 페이지를 요청하셨습니다.</h2>
+        <p class="error__text">페이지의 주소가 잘못 입력되었거나, 변경 혹은 삭제되어 페이지를 찾을 수 없습니다.</p>
       </div>
+
+      <ui-button :type="'button'"
+                 :routerLink="'/'"
+                 :color="'primary'">홈으로 이동
+      </ui-button>
     </div>
   </app-content-wrapper>
 </template>
@@ -30,32 +17,13 @@
 <script>
 export default {
   name: 'AppError',
-  data() {
-    return {
-      postList: [],
-    }
-  },
-  async created() {
+  created() {
     // 페이지 타이틀 세팅
-    this.$store.dispatch('Breadcrumb/FETCH_PAGE_TITLE', '페이지를 찾을 수 없습니다.');
-
-    await this.listPostByLimit(3);
-  },
-  methods: {
-    /** 개수별 포스트 목록 조회 */
-    listPostByLimit(limit) {
-      return this.$http.get(`/post/limit/${limit}`)
-      .then(resp => {
-        resp.data.forEach(d => {
-          this.postList.push(d);
-        });
-      });
-    },
+    this.$store.dispatch('Breadcrumb/FETCH_PAGE_TITLE', '404 ERROR');
   },
 }
 </script>
 
 <style lang="scss" scoped>
 @import '@/assets/scss/views/error.scss';
-@import '@/assets/scss/components/popular.scss';
 </style>
