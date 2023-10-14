215cd3ee5301f181d63fe0a90d7eb93ab89f59c3
selosele <osh3384@naver.com>
2023-10-14 14:57:00 +0900

    빌드 디렉터리명 변경 원복

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ const webpack = require('webpack');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = defineConfig({
-  outputDir: path.resolve(__dirname, '../../backend/selosele2/public'),
+  outputDir: path.resolve(__dirname, '../../backend/selosele2/client'),
   productionSourceMap: false,
   configureWebpack: {
     performance: {
