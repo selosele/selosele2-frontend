2447555014f67318d44cf81d65f1dfbb722a5798
selosele <osh3384@naver.com>
2024-02-07 11:34:25 +0900

    Footer 문구 변경

--- src/components/layout/AppFooter.vue
+++ src/components/layout/AppFooter.vue
@@ -1,10 +1,16 @@
 <template>
   <footer id="footer" class="footer">
     <div class="footer__inner">
-      <p>&copy; 2022 {{ $store.state.BlogConfig.data?.author }}.
+      <!--
+      <p>
+        &copy; 2022 {{ $store.state.BlogConfig.data?.author }}.
         <router-link to="/content/policy" class="link">
           개인정보 수집 및 이용 안내
         </router-link>
+      </p>
+      -->
+      <p>
+        {{ $store.state.BlogConfig.data?.title }}. since 2021 ~
       </p>
     </div>
   </footer>
